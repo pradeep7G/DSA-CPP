4677cf52068812df40eb2dad6ec5b724e6517db2
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-05-05 11:24:09 +0530

    Time: 11 ms (19.22%), Space: 12.2 MB (38.09%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        cout<<next_permutation(begin(nums),end(nums))<<"\n";
+        // cout<<next_permutation(begin(nums),end(nums))<<"\n";
+        //find the longest weakly decreaing suffix (i+1,end), start index = i;
+        //and then find the longest index j such that nums[j]>=nums[i]
+        // and then swap nums[i],nums[j]
+        //and then reverse the suffix (i+1,end)
+        int pivot=-1,n=nums.size();
+        
+        //step-1
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]<nums[i+1])
+                pivot=i;
+        }
+        //setp-2
+        if(pivot==-1)
+            return reverse(begin(nums),end(nums));
+        int minNumIndexSuf=-1;
+        for(int i=pivot+1;i<n;i++)
+        {
+            if(nums[i]>nums[pivot])
+                minNumIndexSuf=i;
+        }
+        //step-3
+        swap(nums[pivot],nums[minNumIndexSuf]);
+        //step-4
+        reverse(begin(nums)+pivot+1,end(nums));
+        
     }
 };
