5b5180f87bdc1adfdd54a524d0f0b04cdbb2f9a1
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-05-05 08:50:31 +0530

    Time: 65 ms (13.39%), Space: 26.5 MB (89.87%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -2,21 +2,46 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         //Time O(NLogN),Space O(N)
-        vector<int> sortedNums(nums);
-        sort(begin(nums),end(nums));
+//         vector<int> sortedNums(nums);
+//         sort(begin(nums),end(nums));
         int n=nums.size();
-        int start=-1,end=-1;
-        for(int i=0;i<n;i++)
+//         int start=-1,end=-1;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(start==-1 && sortedNums[i] != nums[i]){
+//                 start=i;
+//             }
+//             if(start!=-1 && sortedNums[i] != nums[i])
+//             {
+//                 end=i;
+//             }
+//         }
+//         if(start==-1 && end==-1)return 0;
+//         return end-start+1;
+        
+        //Time O(N)
+        int max=nums[0],end=-1;
+        for(int i=1;i<n;i++)
         {
-            if(start==-1 && sortedNums[i] != nums[i]){
-                start=i;
-            }
-            if(start!=-1 && sortedNums[i] != nums[i])
+            if(max>nums[i])
             {
                 end=i;
             }
+            else{
+                max=nums[i];
+            }
         }
-        if(start==-1 && end==-1)return 0;
+        int min=nums[n-1],start=0;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(min<nums[i])
+            {
+                start=i;
+            }
+            else{
+                min=nums[i];
+            }
+        }
         return end-start+1;
     }
 };
