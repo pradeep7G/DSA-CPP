ffc0c4a50eacc99ab264ef8f2441d7a747f968d2
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-05-05 10:24:33 +0530

    Time: 38 ms (36.51%), Space: 31.1 MB (85.28%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,20 +2,37 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        vector<int> prefSums(n+1,0),sufSums(n+1,0);
-        for(int i=1;i<n;i++)
+        //space : O(n),can you reduce space complexity??
+        // vector<int> prefSums(n+1,0),sufSums(n+1,0);
+        // for(int i=1;i<n;i++)
+        // {
+        //     prefSums[i]=prefSums[i-1]+nums[i-1];
+        // }
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     sufSums[i]=sufSums[i+1]+nums[i+1];
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     cout<<prefSums[i]<<" "<<sufSums[i]<<"\n";
+        //     if(prefSums[i]==sufSums[i])
+        //         return i;
+        // }
+        // return -1;
+        int leftSum=0,rightSum=0;
+        for(int i=0;i<n;i++)
         {
-            prefSums[i]=prefSums[i-1]+nums[i-1];
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            sufSums[i]=sufSums[i+1]+nums[i+1];
+            rightSum+=nums[i];
         }
         for(int i=0;i<n;i++)
         {
-            cout<<prefSums[i]<<" "<<sufSums[i]<<"\n";
-            if(prefSums[i]==sufSums[i])
+            leftSum+=nums[i];
+            if(leftSum==rightSum)
                 return i;
+            else
+            {
+                rightSum-=nums[i];
+            }
         }
         return -1;
     }
