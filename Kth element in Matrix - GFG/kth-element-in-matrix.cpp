b8b253eca2abb22c348356c2efe1e63f02aace6b
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-07 14:49:17 +0530

    Added solution - LeetHub

--- Kth element in Matrix - GFG/kth-element-in-matrix.cpp
+++ Kth element in Matrix - GFG/kth-element-in-matrix.cpp
@@ -34,28 +34,43 @@ int kthSmallest(int mat[MAX][MAX], int n, int k)
     //10 ---> 50 , find num of elements less than mid1 , if nelems < k,left= mid1+1 else right-mid1-1;
     // we will stop at right value which is kth smallest and exists in the matrix;
     
-    int l=mat[0][0],r=mat[n-1][n-1];
+    // int l=mat[0][0],r=mat[n-1][n-1];
+    // while(l<=r){
+    //     int mid1=l+(r-l)/2;
+    //     int count=0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         int low=0,high=n-1;
+    //         while(low<=high)
+    //         {
+    //             int mid=low+(high-low)/2;
+    //             if(mat[i][mid]<=mid1){
+    //                 low=mid+1;
+    //             }
+    //             else{
+    //                 high=mid-1;
+    //             }
+    //         }
+    //         count+=low;
+    //     }
+    //     if(count<k)l=mid1+1;
+    //     else
+    //         r=mid1-1;
+    // }
+    // return l;
+    
+    int l=0,r=10000;
     while(l<=r){
         int mid1=l+(r-l)/2;
         int count=0;
         for(int i=0;i<n;i++)
         {
-            int low=0,high=n-1;
-            while(low<=high)
-            {
-                int mid=low+(high-low)/2;
-                if(mat[i][mid]<=mid1){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
-            count+=low;
+            count+=(upper_bound(mat[i],mat[i]+n,mid1)-mat[i]);
         }
-        if(count<k)l=mid1+1;
+        if(count<k)
+        l=mid1+1;
         else
-            r=mid1-1;
+        r=mid1-1;
     }
     return l;
 }
