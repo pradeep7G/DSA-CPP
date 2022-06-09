67e4154c61b25108f00465f4e2903eda9f4e98d1
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-09 10:53:01 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -18,9 +18,31 @@ class Solution {
         return dp[i][j]=solve(i+1,n,j+1,m,s,t);
        return dp[i][j]=1+min(solve(i,n,j+1,m,s,t),min(solve(i+1,n,j,m,s,t),solve(i+1,n,j+1,m,s,t)));
   }
+  int solveiter(int n,int m,string s,string t){
+      int dp[n+1][m+1];
+      for(int i=0;i<=n;i++)
+      {
+          dp[i][0]=i;
+      }
+      for(int i=0;i<=m;i++){
+          dp[0][i]=i;
+      }
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+              if(s[i-1]==t[j-1]){
+                  dp[i][j]=dp[i-1][j-1];
+              }
+              else
+              {
+                  dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+              }
+          }
+      }
+      return dp[n][m];
+  }
     int editDistance(string s, string t) {
         memset(dp,-1,sizeof dp);
-       return solve(0,s.length(),0,t.length(),s,t);
+       return solveiter(s.length(),t.length(),s,t);
     }
 };
 
