345a6e62374771230e6c0d66dfcb74d124d25475
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-12 14:21:20 +0530

    Added solution - LeetHub

--- Height of Binary Tree - GFG/height-of-binary-tree.cpp
+++ Height of Binary Tree - GFG/height-of-binary-tree.cpp
@@ -105,6 +105,25 @@ class Solution{
         // int left=height(node->left);
         // int right=height(node->right);
         // return 1+max(left,right);
+        
+        //iterative
+        
+        queue<Node *> q;
+        q.push(node);
+        int count=0;
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Node* front=q.front();
+                q.pop();
+                if(front->left)
+                    q.push(front->left);
+                if(front->right)
+                    q.push(front->right);
+            }
+            count++;
+        }
+        return count;
     }
 };
 
