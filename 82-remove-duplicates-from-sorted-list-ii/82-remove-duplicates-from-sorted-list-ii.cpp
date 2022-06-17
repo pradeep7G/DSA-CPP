ab2369a0bb17faa7cdfc2a2fa9136563a9129761
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-17 15:52:34 +0530

    Time: 28 ms (5.98%), Space: 11.2 MB (17.98%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,41 +10,59 @@
  */
 class Solution {
 public:
+    ListNode* recurse(ListNode *root){
+        if(!root)
+            return NULL;
+        if(root->next==NULL)
+            return root;
+        auto next=root->next;
+        if(root->val!=next->val){
+            root->next=recurse(next);
+            return root;
+        }
+        else{
+            while(next && root->val==next->val){
+                next=next->next;
+            }
+            return recurse(next);
+        }
+        return root;
+    }
     ListNode* deleteDuplicates(ListNode* head) {
         if(!head || head->next==NULL)
             return head;
-       ListNode *dummy=new ListNode();
-       ListNode *cur=head,*newHead=dummy;
-       while(cur){
-           if(cur->next && cur->val!=cur->next->val){
-               if(dummy->next==NULL)
-               {  
-                   dummy->next=cur;
-                   newHead=cur;
-               }
-               else{
-                   newHead->next=cur;
-                   newHead=cur;
-               }
-               cur=cur->next;
-           }
-           else{
-               if(cur->next)
-               {
-                ListNode *prev=cur;
-                cur=cur->next;
-                while(cur && prev->val==cur->val)
-                    cur=cur->next;
-               }
-               else{
-                   newHead->next=cur;
-                   newHead=cur;
-                   cur=cur->next;
-               }
-           }
-       }
-        if(newHead)
-        newHead->next=NULL;
-        return dummy->next;
+       // ListNode *dummy=new ListNode();
+       // ListNode *cur=head,*newHead=dummy;
+       // while(cur){
+       //     if(cur->next && cur->val!=cur->next->val){
+       //         if(dummy->next==NULL)
+       //         {  
+       //             dummy->next=cur;
+       //             newHead=cur;
+       //         }
+       //         else{
+       //             newHead->next=cur;
+       //             newHead=cur;
+       //         }
+       //         cur=cur->next;
+       //     }
+       //     else{
+       //         if(cur->next)
+       //         {
+       //          ListNode *prev=cur;
+       //          cur=cur->next;
+       //          while(cur && prev->val==cur->val)
+       //              cur=cur->next;
+       //         }
+       //         else{
+       //             newHead->next=cur;
+       //             newHead=cur;
+       //             cur=cur->next;
+       //         }
+       //     }
+       // }
+       //  if(newHead)
+       //  newHead->next=NULL;
+        return recurse(head);
     }
 };
