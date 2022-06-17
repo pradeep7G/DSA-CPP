99b3e97a5563180990d319ffe093c35eecdeb501
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-17 14:55:48 +0530

    Time: 18 ms (77.76%), Space: 11.5 MB (30.40%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -10,7 +10,25 @@
  */
 class Solution {
 public:
-    
+    ListNode *reverseKgroups(ListNode *head,int k,int lenofll){
+        if(!head)
+            return NULL;
+        if(lenofll<k)
+            return head;
+        ListNode *cur=head,*prev=NULL,*next=NULL;
+        int count=0;
+        while(cur!=NULL && count<k){
+            next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
+            count++;
+        }
+        
+        if(next!=NULL && lenofll>=k)
+            head->next=reverseKgroups(next,k,lenofll-k);
+        return prev;
+    }
     ListNode* reverseKGroup(ListNode* head, int k) {
         if(!head || k==1)
             return head;
@@ -21,34 +39,34 @@ public:
             lenofll++;
         }
         ListNode *dummy=new ListNode();
-        
-        //do for the first group
-        ListNode *prev=NULL,*cur=head,*link=head,*link2;
-        int cnt=k;
-        for(int i=0;i<cnt;i++){
-            ListNode *temp=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=temp;
-        }
-        dummy->next=prev;
-        //do for the rest of the groups
-        lenofll-=k;
-        while(lenofll>=k){
-            prev=NULL,cur=cur;
-            link2=cur;
-            for(int i=0;i<cnt;i++){
-                ListNode *temp=cur->next;
-                cur->next=prev;
-                prev=cur;
-                cur=temp;
-            }
-            lenofll-=k;
-            link->next=prev;
-            link=link2;
-        }
-        if(lenofll!=0 && lenofll<k)
-            link->next=cur;
-        return dummy->next;
+        return reverseKgroups(head,k,lenofll);
+        // //do for the first group
+        // ListNode *prev=NULL,*cur=head,*link=head,*link2;
+        // int cnt=k;
+        // for(int i=0;i<cnt;i++){
+        //     ListNode *temp=cur->next;
+        //     cur->next=prev;
+        //     prev=cur;
+        //     cur=temp;
+        // }
+        // dummy->next=prev;
+        // //do for the rest of the groups
+        // lenofll-=k;
+        // while(lenofll>=k){
+        //     prev=NULL,cur=cur;
+        //     link2=cur;
+        //     for(int i=0;i<cnt;i++){
+        //         ListNode *temp=cur->next;
+        //         cur->next=prev;
+        //         prev=cur;
+        //         cur=temp;
+        //     }
+        //     lenofll-=k;
+        //     link->next=prev;
+        //     link=link2;
+        // }
+        // if(lenofll!=0 && lenofll<k)
+        //     link->next=cur;
+        // return dummy->next;
     }
 };
