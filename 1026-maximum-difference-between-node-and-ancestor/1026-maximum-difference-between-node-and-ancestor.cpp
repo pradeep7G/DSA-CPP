38f4cc280a05f791b29655e70d206b3b78c932c6
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-19 22:51:21 +0530

    Time: 13 ms (25.12%), Space: 16.9 MB (5.22%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -13,35 +13,17 @@ class Solution {
 public:
     vector<int> findmaxdiff(TreeNode *root,int &ans){
         if(!root){
-            return {};
-        }
-        if(root->left==NULL && root->right==NULL)
-        {
-            return {root->val,root->val};
+            return {INT_MAX,INT_MIN};
         }
         vector<int> left=findmaxdiff(root->left,ans);
         vector<int> right=findmaxdiff(root->right,ans);
-        if(left.size()==2){
-            ans=max({ans,abs(root->val - left[0]),abs(root->val-left[1])});
-        }
-        if(right.size()==2){
-            ans=max({ans,abs(root->val-right[0]),abs(root->val-right[1])});
-        }
-        if(left.size()==2 && right.size()==2){
-            return {min({left[0],right[0],root->val}),max({left[1],right[1],root->val})};
-        }
-        else{
-            if(left.size()==2){
-                return {min(left[0],root->val),max(root->val,left[1])};
-            }
-            else{
-                return {min(right[0],root->val),max(root->val,right[1])};
-            }
-        }
-        return {INT_MAX,INT_MIN};
+        auto curmin=min({root->val,left[0],right[0]});
+        auto curmax=max({root->val,left[1],right[1]});
+        ans=max({ans,abs(root->val-curmax),abs(root->val-curmin)});
+        return {curmin,curmax};
     }
     int maxAncestorDiff(TreeNode* root) {
-        int ans=INT_MIN;
+        int ans=0;
         auto res=findmaxdiff(root,ans);
         return ans;
     }
