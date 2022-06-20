7cb6e089ca19fbc3555f9a109d3f96ae48fd6ea4
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-20 14:59:33 +0530

    Time: 337 ms (46.17%), Space: 123.4 MB (23.68%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -12,32 +12,57 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         //find mid compare , reverse and return;
-        ListNode* slow=head,*fast=head->next;
+        // //OR traverse upto mid and store values in stack, and start popping from after mid, if at any point not equal return false, else true;
+        // ListNode* slow=head,*fast=head->next;
+        // while(fast && fast->next){
+        //     slow=slow->next;
+        //     fast=fast->next->next;
+        // }
+        // bool flag=true;
+        // if(fast==NULL){
+        //     flag=false;
+        // }
+        // ListNode* reverse=slow->next;
+        // slow->next=NULL;
+        // ListNode* cur=reverse;
+        // ListNode* prev=NULL;
+        // while(cur){
+        //     ListNode* temp=cur->next;
+        //     cur->next=prev;
+        //     prev=cur;
+        //     cur=temp;
+        // }
+        // ListNode *l1=head,*l2=prev;
+        // bool ispal=false;
+        // while(l1 && l2){
+        //     if(l1->val != l2->val)
+        //         return false;
+        //     l1=l1->next;l2=l2->next;
+        // }
+        // return true;
+        ListNode *slow=head,*fast=head->next;
+        stack<ListNode *> st;
+        st.push(head);
         while(fast && fast->next){
             slow=slow->next;
+            st.push(slow);
             fast=fast->next->next;
         }
-        bool flag=true;
         if(fast==NULL){
-            flag=false;
+            st.pop();
         }
-        ListNode* reverse=slow->next;
-        slow->next=NULL;
-        ListNode* cur=reverse;
-        ListNode* prev=NULL;
-        while(cur){
-            ListNode* temp=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=temp;
-        }
-        ListNode *l1=head,*l2=prev;
-        bool ispal=false;
-        while(l1 && l2){
-            if(l1->val != l2->val)
+        ListNode *cur=slow->next;
+        while(cur && !st.empty()){
+            if(cur->val != st.top()->val){
                 return false;
-            l1=l1->next;l2=l2->next;
+            }
+            else{
+                cur=cur->next;
+                st.pop();
+            }
         }
+        if(!st.empty())
+            return false;
         return true;
     }
 };
