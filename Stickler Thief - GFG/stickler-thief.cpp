4948c59f991b684f3aa889a10f10f33d49099ccc
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-24 10:52:45 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -12,10 +12,9 @@ class Solution
     {
         vector<int> dp(n);
         dp[0]=a[0];
-        dp[1]=max(a[1],a[0]);
-        
+        dp[1]=max(dp[0],a[1]);
         for(int i=2;i<n;i++){
-            dp[i]=max(a[i]+dp[i-2],dp[i-1]);
+            dp[i]=max(dp[i-2]+a[i],dp[i-1]);
         }
         return dp[n-1];
     }
