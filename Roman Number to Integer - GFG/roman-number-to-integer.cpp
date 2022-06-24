b9bcd55894bd25afcc2fa17e51183572c5bae65d
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-24 20:54:20 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.cpp
+++ Roman Number to Integer - GFG/roman-number-to-integer.cpp
@@ -11,25 +11,44 @@ using namespace std;
 class Solution {
   public:
     int romanToDecimal(string &str) {
-       int ans=0;
-       unordered_map<char,int> num;
-       num['I'] = 1;
-       num['V'] = 5;
-       num['X'] = 10;
-       num['L'] = 50;
-       num['C'] = 100;
-       num['D'] = 500;
-       num['M'] = 1000;
+    //   int ans=0;
+    //   unordered_map<char,int> num;
+    //   num['I'] = 1;
+    //   num['V'] = 5;
+    //   num['X'] = 10;
+    //   num['L'] = 50;
+    //   num['C'] = 100;
+    //   num['D'] = 500;
+    //   num['M'] = 1000;
        
-       for(int i=str.length()-1;i>=0;i--)
-       {
-           if(i<str.length()-1 && num[str[i]]<num[str[i+1]]){
-               ans-=num[str[i]];
-           }else{
-               ans+=num[str[i]];
-           }
-       }
-       return ans;
+    //   for(int i=str.length()-1;i>=0;i--)
+    //   {
+    //       if(i<str.length()-1 && num[str[i]]<num[str[i+1]]){
+    //           ans-=num[str[i]];
+    //       }else{
+    //           ans+=num[str[i]];
+    //       }
+    //   }
+    //   return ans;
+    int ans=0;
+    unordered_map<char,int> num;
+    num['I']=1;
+    num['V']=5;
+    num['X']=10;
+    num['L']=50;
+    num['C']=100;
+    num['D']=500;
+    num['M']=1000;
+    int n=str.length();
+    for(int i=n-1;i>=0;i--){
+        if(i<n-1 && num[str[i]]< num[str[i+1]]){
+            ans-=num[str[i]];
+        }
+        else{
+            ans+=num[str[i]];
+        }
+    }
+    return ans;
     }
 };
 
