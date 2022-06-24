61dc4b44aa52ac57a83e82e81432a607f2918bd7
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-24 10:11:40 +0530

    Time: 4 ms (70.25%), Space: 11 MB (74.84%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -41,19 +41,26 @@ public:
         lo=0,hi=n-1;
         //assume start as pivot and again to binary search to find target; or do two binary searches in first part and second part.
         
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            int realmid=(mid+pivot)%n;
-            if(nums[realmid]==target)
-            {
-                return realmid;
-            }
-            if(nums[realmid]<target){
-                lo=mid+1;
-            }
-            else
-                hi=mid-1;
-        }
+        // while(lo<=hi){
+        //     int mid=lo+(hi-lo)/2;
+        //     int realmid=(mid+pivot)%n;
+        //     if(nums[realmid]==target)
+        //     {
+        //         return realmid;
+        //     }
+        //     if(nums[realmid]<target){
+        //         lo=mid+1;
+        //     }
+        //     else
+        //         hi=mid-1;
+        // }
+        // return -1;
+        int ret=lower_bound(nums.begin(),nums.begin()+pivot,target)-nums.begin();
+        if(ret>=0 && ret<n && nums[ret]==target)
+            return ret;
+        ret=lower_bound(begin(nums)+pivot,end(nums),target)-begin(nums);
+        if(ret>=0 && ret<n &&  nums[ret]==target)
+            return ret;
         return -1;
     }
 };
