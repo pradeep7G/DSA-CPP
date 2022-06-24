b1a55da4ca35885d48177d44616da2f5d36d0dac
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-24 13:33:42 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -6,21 +6,50 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    int kthElement(int arr1[], int arr2[], int n, int m, int kreq)
     {
-       int lo=min(arr1[0],arr2[0]);
-       int hi=max(arr1[n-1],arr2[m-1]);
-       while(lo<=hi){
-           int mid=lo+(hi-lo)/2;
-           int x=upper_bound(arr1,arr1+n,mid)-arr1;
-           int y=upper_bound(arr2,arr2+m,mid)-arr2;
-           if(x+y < k){
-               lo=mid+1;
-           }
-           else
-                hi=mid-1;
-       }
-       return lo;
+        
+    //   int lo=min(arr1[0],arr2[0]);
+    //   int hi=max(arr1[n-1],arr2[m-1]);
+    //   while(lo<=hi){
+    //       int mid=lo+(hi-lo)/2;
+    //       int x=upper_bound(arr1,arr1+n,mid)-arr1;
+    //       int y=upper_bound(arr2,arr2+m,mid)-arr2;
+    //       if(x+y < k){
+    //           lo=mid+1;
+    //       }
+    //       else
+    //             hi=mid-1;
+    //   }
+    //   return lo;
+    int k=0,i=0,j=0;
+        while(i<n && j<m){
+            if(arr1[i]<arr2[j]){
+                k++;
+                if(k==kreq){
+                    return arr1[i];
+                }
+                i++;
+            }else{
+                k++;
+                if(k==kreq){
+                    return arr2[j];
+                }
+                j++;
+            }
+        }
+        while(i<n){
+            k++;
+            if(k==kreq){
+                return arr1[i];            }
+            i++;
+        }
+        while(j<m){
+            k++;
+            if(k==kreq)
+             return arr2[j];
+             j++;
+        }
     }
 };
 
