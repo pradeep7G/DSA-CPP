f62a1a32e2ed9fe77e069c0ba001c08c1484490d
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-24 18:36:03 +0530

    Time: 4 ms (88.21%), Space: 7.1 MB (33.38%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string build(string str){
+        unordered_map<char,int> mp;
+        int n=str.length();
+        int i=0;
+        string ret="";
+        while(i<n){
+            int j=i,count=1;
+            while(j+1 <n && str[j]==str[j+1]){
+                count++;
+                j++;
+            }
+            j++;
+            ret+=to_string(count)+str[i];
+            i=j;
+        }
+        return ret;
+    }
+    string countAndSay(int n) {
+        string ans="1";
+        if(n==1)
+            return ans;
+        ans=countAndSay(n-1);
+        ans=build(ans);
+        return ans;
+    }
+};
