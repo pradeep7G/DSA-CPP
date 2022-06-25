06f157236df7ceeb115e84349a68e6dd4a299b7c
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-25 12:40:36 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -15,6 +15,7 @@ class Solution
         for(int i=0;i<n;i++){
             v.push_back({start[i],end[i]});
         }
+        //Sorting by start time  and handling edge cases like 1 - 6 and 3 - 4.
         // sort(v.begin(),v.end());
         // mergedvals.push_back(v[0]);
         // for(int i=1;i<n;i++){
@@ -36,26 +37,45 @@ class Solution
         
         //sort by end time and find count;
         
-        sort(v.begin(),v.end(),[&](auto a,auto b){
-           if(a.second==b.second)   
-            return a.first > b.first;
-           if(a.second < b.second)
-            return true;
-            else
-                return false;
-        });
+        //Sorting by end time and no need to take care of 1 - 6 and 3 - 4 coz 3 - 4 will be the one than the latter as to have maximum meetings  we choose meetings that ends early
+        // sort(v.begin(),v.end(),[&](auto a,auto b){
+        //   if(a.second==b.second)   
+        //     return a.first > b.first;
+        //   if(a.second < b.second)
+        //     return true;
+        //     else
+        //         return false;
+        // });
         
-        int cnt=1;
+        // int cnt=1;
+        // auto prev=v[0];
+        // for(int i=1;i<n;i++){
+        //     if(v[i].first > prev.second)
+        //     {
+        //         cnt++;
+        //         prev=v[i];
+        //         continue;
+        //     }
+        // }
+        // return cnt;
+        
+        //Practice
+        sort(v.begin(),v.end(),[&](auto a,auto b){
+           if(a.second==b.second) {
+               a.first > b.first;
+           }
+           return a.second < b.second;
+        });
+        int count=1;
         auto prev=v[0];
         for(int i=1;i<n;i++){
-            if(v[i].first > prev.second)
-            {
-                cnt++;
-                prev=v[i];
-                continue;
+            auto cur=v[i];
+            if(cur.first > prev.second){
+                count++;
+                prev=cur;
             }
         }
-        return cnt;
+        return count;
     }
 };
 
