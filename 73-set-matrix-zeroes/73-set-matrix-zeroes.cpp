a43454ab77a0d562da7f189484d6e63ba472cc63
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-27 11:39:26 +0530

    Time: 32 ms (15.37%), Space: 13.2 MB (81.66%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,22 +4,12 @@ public:
         bool rowFlag=false,colFlag=false;
         int m=matrix.size(),n=matrix[0].size();
         for(int i=0;i<m;i++){
-            if(matrix[i][0]==0)
-            {
-                colFlag=true;
-                break;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(matrix[0][i]==0)
-            {  
-                rowFlag=true;
-                break;
-            }
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
+                    if(j==0)
+                        colFlag=true;
+                    if(i==0)
+                        rowFlag=true;
                     matrix[i][0]=0;
                     matrix[0][j]=0;
                 }
