f9d2064a7ac16e62dae1df73ec8534df0de46612
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-27 12:44:38 +0530

    Time: 426 ms (5.04%), Space: 47.2 MB (6.09%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -5,18 +5,17 @@ public:
        sort(i.begin(),i.end(),[&](auto a,auto b){
           return a[0]<b[0];
        });
-       v.push_back(i[0]);
        auto prev=i[0];
        for(int j=1;j<i.size();j++){
-           if(prev[1]<i[j][0]){
-               v.push_back(i[j]);
-               prev=i[j];
+           if(prev[1]>=i[j][0]){
+               prev[1]=max(prev[1],i[j][1]);
            }
            else{
-               v.back()[1]=max(v.back()[1],i[j][1]);
-               prev=v.back();
+              v.push_back(prev);
+              prev=i[j];
            }
        }
+        v.push_back(prev);
        return v;
     }
 };
