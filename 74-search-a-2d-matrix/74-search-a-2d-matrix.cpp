bca67b6ae98676cb92669767bd0f3c71530914ab
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-27 14:02:09 +0530

    Time: 5 ms (63.74%), Space: 9.6 MB (5.23%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,57 +2,11 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         // on search on rows and one on columns
-        // if(mat.size()==0 || mat[0].size()==0)
-        //     return false;
-        // int rows=mat.size(),cols=mat[0].size();
-        // int low=0,high=rows-1;
-        // while(low<=high){
-        //     int mid=low+(high-low)/2;
-        //     int cur=mat[mid][0];
-        //     if(cur==target)
-        //         return true;
-        //     if(cur<target)
-        //         low=mid+1;
-        //     else
-        //         high=mid-1;
-        // }
-        // int h=high;
-        // low=0,high=cols-1;
-        // if(h>=0)
-        // {
-        //  while(low<=high){
-        //     int mid=low+(high-low)/2;
-        //     int cur=mat[h][mid];
-        //     if(cur==target)
-        //         return true;
-        //     if(cur<target)
-        //         low=mid+1;
-        //     else
-        //         high=mid-1;
-        // }   
-        // }
-        // return false;
-        
-        //treating as continous sorted list
-        
-        // int low=0,high=rows*cols - 1;
-        // while(low<=high){
-        //     int mid=low+(high-low)/2;
-        //     int cur=mat[mid/cols][mid%cols];
-        //     if(cur==target)
-        //         return true;
-        //     else if(cur<target)
-        //     {
-        //         low=mid+1;
-        //     }
-        //     else
-        //         high=mid-1;
-        // }
-        // return false;
-        
-        if(mat.size()==0 || mat[0].size()==0)
+         if(mat.size()==0 || mat[0].size()==0)
             return false;
         int rows=mat.size(),cols=mat[0].size();
+       
+        /*
         int lo=0,hi=rows-1,row=-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
@@ -79,6 +33,22 @@ public:
                 else
                     hi=mid-1;
             }
+        }
+        return false;
+        */
+        
+        //treating as continous sorted list
+        int lo=0,hi=rows*cols-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int r=mid/cols,c=mid%cols;
+            int cur=mat[r][c];
+            if(cur==target)
+                return true;
+            if(cur<target)
+                lo=mid+1;
+            else
+                hi=mid-1;
         }
         return false;
     }
