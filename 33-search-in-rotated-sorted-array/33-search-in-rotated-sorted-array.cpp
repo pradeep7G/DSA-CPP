3e4f29b6d67ee64165d737b2cbd71bc968ce1358
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-28 17:11:55 +0530

    Time: 3 ms (87.58%), Space: 11.1 MB (29.04%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -33,6 +33,30 @@ public:
         }
         return -1;
     }
+    int single_binary_search(vector<int> &nums,int target){
+        int n=nums.size();
+        int lo=0,hi=n-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]>nums[n-1])
+                lo=mid+1;
+            else
+                hi=mid-1;
+        }
+        int rot=lo;
+        lo=0,hi=n-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int realmid=(mid+rot)%n;
+            if(nums[realmid]==target)
+                return realmid;
+            else if(nums[realmid]<target)
+                lo=mid+1;
+            else
+                hi=mid-1;
+        }
+        return -1;
+    }
     int search(vector<int>& nums, int target) {
         // int n=nums.size();
         // int low=0,high=n-1;
@@ -56,7 +80,7 @@ public:
         //         hi=mid-1;
         // }
         // return -1;
-        return two_binary_search(nums,target);
+        return single_binary_search(nums,target);
         int n=nums.size();
         int lo=0,hi=n-1;
         while(lo<=hi){
