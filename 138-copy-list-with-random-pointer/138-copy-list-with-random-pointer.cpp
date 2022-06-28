a018e42f22d2b38a44beffa8c8853d2ecd38eb9f
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-06-28 11:07:29 +0530

    Time: 8 ms (84.81%), Space: 11.3 MB (59.75%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -16,19 +16,21 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
+    Node* cloneList(Node *head){
+        //Two pass, Single Pass??
         unordered_map<Node *,Node *> mp;
-        Node *cloneHead=NULL,*cloneTail=NULL,*cur=head;
+        Node *cloneHead=NULL,*cloneTail=NULL;
+        Node *cur=head;
         while(cur){
-            Node *newNode=new Node(cur->val);
-            mp[cur]=newNode;
+            Node *newnode=new Node(cur->val);
+            mp[cur]=newnode;
             if(cloneHead==NULL)
             {
-                cloneHead=newNode;
-                cloneTail=newNode;
+                cloneHead=newnode;
+                cloneTail=newnode;
             }
             else{
-                cloneTail->next=newNode;
+                cloneTail->next=newnode;
                 cloneTail=cloneTail->next;
             }
             cur=cur->next;
@@ -40,6 +42,63 @@ public:
             tmp=tmp->next;
             cur=cur->next;
         }
+        return cloneHead;
+    }
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node *,Node *> mp;
+        Node *cloneHead=NULL,*cloneTail=NULL,*cur=head;
+        while(cur){
+            Node *newNode=NULL;
+            if(mp.find(cur)!=mp.end()){
+                newNode=mp[cur];
+            }
+            else
+            {   
+                newNode=new Node(cur->val);
+                mp[cur]=newNode;
+            }
+            if(cloneHead==NULL)
+            {
+                cloneHead=newNode;
+                cloneTail=newNode;
+                if(mp.find(cur->random)!=mp.end()){
+                    cloneHead->random=mp[cur->random];
+                    cloneTail->random=mp[cur->random];
+                }
+                else{
+                    if(cur->random)
+                    {
+                    Node *node=new Node(cur->random->val);
+                    mp[cur->random]=node;
+                    cloneHead->random=mp[cur->random];
+                    cloneTail->random=mp[cur->random];
+                    }
+                }
+            }
+            else{
+                cloneTail->next=newNode;
+                cloneTail=cloneTail->next;
+                if(mp.find(cur->random)!=mp.end()){
+                    cloneTail->random=mp[cur->random];
+                }
+                else{
+                     if(cur->random)
+                    {
+                    Node *node=new Node(cur->random->val);
+                    mp[cur->random]=node;
+                    cloneTail->random=mp[cur->random];
+                     }
+                }
+            }
+            cur=cur->next;
+        }
+        // cur=head;
+        // Node *tmp=cloneHead;
+        // while(cur && tmp){
+        //     tmp->random=mp[cur->random];
+        //     tmp=tmp->next;
+        //     cur=cur->next;
+        // }
         return cloneHead;
     }
 };
