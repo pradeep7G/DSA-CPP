709b405f31053e15f5b925813ed08e70e510962b
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-02 21:17:40 +0530

    Time: 24 ms (95.11%), Space: 11 MB (31.94%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+    int singlepass(string ans,int k){
+        int count_t=0,count_f=0,res=0;
+        int left=0;
+        for(int right=0;right<ans.length();right++){
+            if(ans[right]=='T')
+                count_t++;
+            else
+                count_f++;
+            while(min(count_t,count_f)>k){
+                if(ans[left++]=='T')
+                    count_t--;
+                else
+                    count_f--;
+            }
+            res=max(res,right-left+1);
+        }
+        return res;
+    }
     int maxConsecutiveAnswers(string ans, int k) {
+        return singlepass(ans,k);
         int trues=0;
         int left=0,right=0,n=ans.length();
         int res=0;
