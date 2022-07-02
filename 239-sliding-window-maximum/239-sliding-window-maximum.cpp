d586d04a49f86a615ff35f59a51e9c445007c911
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-02 07:27:24 +0530

    Time: 288 ms (97.86%), Space: 134.6 MB (77.35%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
+    // O(N)??
+    vector<int> solve(vector<int> &nums,int k){
+        deque<int> dq;
+        vector<int> ans;
+        int n=nums.size();
+        for(int i=0;i<k;i++){
+            while(!dq.empty() && nums[dq.back()]<nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+        }
+        ans.push_back(nums[dq.front()]);
+        for(int i=k;i<n;i++){
+            while(!dq.empty() && i-dq.front()>=k)
+                dq.pop_front();
+            while(!dq.empty() && nums[dq.back()]<nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        //O(NLOGN)
+        return solve(nums,k);
         priority_queue<pair<int,int>> pq;
         int n=nums.size();
         vector<int> ans;
