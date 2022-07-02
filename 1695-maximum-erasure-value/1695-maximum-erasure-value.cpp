caf65f9e63623e0d6f45aac015bc94766855c293
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-02 21:31:29 +0530

    Time: 635 ms (36.33%), Space: 114.2 MB (50.12%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
+    int solve(vector<int> &nums){
+        int sum=0;
+        int n=nums.size();
+        int left=0,right=0,res=0;
+        unordered_map<int,int> mp;
+        for(;right<n;right++){
+            sum+=nums[right];
+            mp[nums[right]]++;
+            while(mp.size()<right-left+1){
+                int num=nums[left++];
+                sum-=num;
+                mp[num]--;
+                if(mp[num]==0)
+                    mp.erase(num);
+            }
+            if(mp.size()==right-left+1)
+            res=max(res,sum);
+        }
+        return res;
+    }
     int maximumUniqueSubarray(vector<int>& nums) {
         //subarray containing unqiue elements and score is it's sum;
+        return solve(nums);
         int sum=0;
         int n=nums.size();
         int left=0,right=0,res=0;
