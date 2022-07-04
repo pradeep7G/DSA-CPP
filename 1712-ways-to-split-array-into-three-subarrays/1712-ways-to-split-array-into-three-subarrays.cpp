a739b27f02d61626d7bf2af6b51058a6816380c7
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-04 13:33:57 +0530

    Time: 166 ms (95.76%), Space: 85 MB (38.25%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -1,11 +1,35 @@
 class Solution {
+    
+    /* 
+        ~ discussion ~
+        
+        ~ presums --> pre[i]<=pre[j]<=pre[k] (0,i)<=(i,j)<=(j,k); //i = left_boundary, j=right_boundary
+        ~ left sum --  i_middle sum_j -- right sum
+        we are searching for smallest point that satisfies condition and the largest point that satisfies the condition,         so if we find the largest that means we can remove an element from middle sum and add it to the right sum and we         can do this until smallest possible i constraint, so we add j-i to the result, that's how we are                         optimizing/pruning the search by calculating smallest and largest possiblities of left boundary satisfying the           predicate  
+    */
+    
+    
 public:
     int MOD=1e9+7;
+    int singlepass_pointers(vector<int> &nums){
+        //2*prefix[i]<=pref[j] and pre[j]<=(pref[i]+pref[n])/2 
+        // We use pointers to validate those conditions
+        int n=nums.size();
+        vector<int> pre(n,0);
+        partial_sum(nums.begin(),nums.end(),pre.begin());
+        long long res=0;
+        int i=0,j=0,k=0;
+        for(int i=0;i<n-2;i++){
+            while(j<=i || (j<n-1 && pre[j] < 2*pre[i]))
+                j++;
+            while(k<j || (k<n-1 && 2*pre[k] <= pre[i] + pre[n-1]))
+                k++;
+            res=(res+k-j)%MOD;
+        }
+        return res;
+    }
     int waysToSplit(vector<int>& nums) {
-        //presums --> pre[i]<=pre[j]<=pre[k] (0,i)<=(i,j)<=(j,k); //i = left_boundary, j=right_boundary
-        
-        //left sum --  i_middle sum_j -- right sum
-        //we are searching for smallest point that satisfies condition and the largest point that satisfies the condition, so if we find the largest that means we can remove an element from middle sum and add it to the right sum and we can do this until smallest possible i constraint, so we add j-i to the result, that's how we are optimizing/pruning the search by calculating smallest and largest possiblities of left boundary satisfying the predicate
+        return singlepass_pointers(nums);
         int n=nums.size();
         vector<int> pre(n,0);
         partial_sum(nums.begin(),nums.end(),pre.begin());
