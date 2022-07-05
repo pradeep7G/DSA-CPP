a93109452e2f28c428d660cb61884a3a2fc95ba3
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-05 14:02:38 +0530

    Time: 28 ms (55.27%), Space: 18 MB (18.95%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int allxor=0;
-        for(int i=1;i<=nums.size();i++)
-            allxor^=i;
-        int num_xor=accumulate(begin(nums),end(nums),0,[&](int x,int y){
-            return x^y;
-        });
-        return num_xor ^ allxor;
+        
+        //wow, this xor trick seems super interesting
+        
+        /*
+            int allxor=0;
+            for(int i=1;i<=nums.size();i++)
+                allxor^=i;
+            int num_xor=accumulate(begin(nums),end(nums),0,[&](int x,int y){
+                return x^y;
+            });
+            return num_xor ^ allxor;
+        */
+        
+        //can you do it with cyclic sort?
+        int i=0;
+        int n=nums.size();
+        while(i<n){
+            if(nums[i]<n && nums[i]!=nums[nums[i]]){
+                swap(nums[i],nums[nums[i]]);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i)
+                return i;
+        }
+        return n;
     }
 };
