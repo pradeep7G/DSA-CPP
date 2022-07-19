850612ffd833e1cd80cf6c01472ee91613a79ba0
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-19 15:18:50 +0530

    Time: 446 ms (29.00%), Space: 75.4 MB (27.02%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -14,7 +14,7 @@ public:
             take=solve(ind-1,n,target-nums[ind],nums,dp);
         return dp[ind][target]=take || notTake;
     }
-    bool canPartition(vector<int>& nums) {
+    bool topDown(vector<int> &nums){
         int totSum=accumulate(begin(nums),end(nums),0LL);
         int n=nums.size();
         //s1+s2=totSum && s1=s2 ==> s2=totSum/2;
@@ -22,5 +22,27 @@ public:
             return false;
         vector<vector<int>> dp(n,vector<int>(totSum/2 + 1,-1));
         return solve(n-1,n,totSum/2,nums,dp);
+    }
+    bool canPartition(vector<int>& nums) {
+        int totSum=accumulate(begin(nums),end(nums),0LL);
+        int n=nums.size();
+        //s1+s2=totSum && s1=s2 ==> s2=totSum/2;
+        if(totSum%2)
+            return false;
+        vector<vector<int>> dp(n,vector<int>(totSum/2 + 1,0));
+        for(int i=0;i<n;i++)
+            dp[i][0]=true;
+        if(nums[0]<=totSum/2)
+            dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int target=0;target<=(totSum/2);target++){
+                bool notTake=dp[i-1][target];
+                bool take=false;
+                if(nums[i]<=target)
+                    take=dp[i-1][target-nums[i]];
+                dp[i][target]=take || notTake;
+            }
+        }
+        return dp[n-1][totSum/2];
     }
 };
