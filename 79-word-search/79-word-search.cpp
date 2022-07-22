a56e9ed35de819b159322f753d5da90bb89174b7
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-22 20:12:45 +0530

    Time: 488 ms (57.61%), Space: 8 MB (35.71%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool solve(int i,int j,int ind,int n,string &word,vector<vector<char>> &board){
+        if(ind==n)
+            return true;
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[ind]){
+            return false;
+        }
+        char tmp=board[i][j];
+        board[i][j]='#';
+        bool top=false,down=false,left=false,right=false;
+        top=solve(i-1,j,ind+1,n,word,board);
+        if(!top)
+        down=solve(i+1,j,ind+1,n,word,board);
+        if(!down)
+        left=solve(i,j-1,ind+1,n,word,board);
+        if(!left)
+        right=solve(i,j+1,ind+1,n,word,board);
+        if(top || down || left || right)
+            return true;
+        board[i][j]=tmp; //backtrack
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+       int m=board.size(),n=board[0].size();
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+               if(solve(i,j,0,word.size(),word,board))
+                   return true;
+           }
+       }
+       return false;
+    }
+};
