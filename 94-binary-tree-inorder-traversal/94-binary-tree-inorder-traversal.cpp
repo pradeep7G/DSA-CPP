a1798dec6c0d5e32609b969d4d0a62601c1aa52e
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-26 11:36:00 +0530

    Time: 4 ms (40.78%), Space: 8.4 MB (74.95%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,17 +11,48 @@
  */
 class Solution {
 public:
+    vector<int> morrisTraversal(TreeNode* root){
+        vector<int> inorder;
+        TreeNode* cur=root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            else{
+                //create thread
+                TreeNode* prev=cur->left;
+                while(prev->right!=NULL && prev->right!=cur){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=cur;//connect the thread
+                    cur=cur->left;//Now move left;
+                }
+                else { //if we completed left subtree we again came to the root and we need to remove the thread and add the root to the inorder traversal as we recognized the loop i.e prev->right==cur, remove loop, add cur to inorder, move right;
+                    prev->right=NULL;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
+        }
+        return inorder;
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-        //Iterative
+        //Iterative TC:O(N),SC:O(N)
+        //can you do traversal in O(1)? Morris traversal..
+        return morrisTraversal(root);
         stack<TreeNode *> st;
         vector<int> inorder;
         TreeNode* cur=root;
         while(!st.empty() || cur!=NULL){
             if(cur!=NULL){
+                //add curr to the stack and explore left.
                 st.push(cur);
                 cur=cur->left;
             }
             else{
+                //take the top of the stack and add it the solution, and explore right.
                 cur=st.top();
                 st.pop();
                 inorder.push_back(cur->val);
