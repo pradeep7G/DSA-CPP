2fc0776085020c0a4f431584ed787cb42477d834
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-28 19:14:28 +0530

    Time: 176 ms (8.40%), Space: 8.5 MB (27.60%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -14,9 +14,22 @@ public:
         return dp[i][target]=res;
         
     }
-    int numRollsToTarget(int n, int k, int target) {
+    int memo(int n, int k, int target) {
         //try allways;
         vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
         return solve(n,k,target,dp);
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        //try allways;
+        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=k;j++){
+                for(int k=j;k<=target;k++){
+                    dp[i][k]=(dp[i][k]+dp[i-1][k-j])%mod;
+                }
+            }
+        }
+        return dp[n][target];
     }
 };
