95347cd7b51318c6f86edb9e79ac7e47e4948cb4
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-07-31 21:42:45 +0530

    Time: 48 ms (33.95%), Space: 13.6 MB (60.76%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool bipartite(vector<vector<int>> &graph,int src,vector<int> &colors){
+    bool bipartiteBFS(vector<vector<int>> &graph,int src,vector<int> &colors){
         queue<int> q;
         q.push(src);
         colors[src]=1;
@@ -19,13 +19,46 @@ public:
         }
         return false;
     }
+    bool bipartiteDFS(vector<vector<int>> &graph,int i,vector<int> &colors,vector<bool> &vis,int parentColor){
+        vis[i]=true;
+        for(auto j:graph[i]){
+            if(!vis[j]){
+                // if(!colors[j]){
+                    colors[j]=-parentColor;
+                // }
+                // else if(colors[j]==colors[i]){
+                //     return true;
+                // }
+                if(bipartiteDFS(graph,j,colors,vis,colors[j]))
+                    return true;
+            }
+            else if(colors[j]==colors[i]){
+                    return true;
+            }
+        }
+        return false;
+    }
+    bool isBipartiteDfs(vector<vector<int>> &graph){
+        int n=graph.size();
+        vector<int> colors(n,0);
+        vector<bool> vis(n,false);
+        for(int i=0;i<n;i++){
+            if(colors[i])
+                continue;
+            colors[i]=1;
+            if(bipartiteDFS(graph,i,colors,vis,colors[i]))
+                return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
+        return isBipartiteDfs(graph);
         int n=graph.size();
         vector<int> colors(n,0);
         for(int i=0;i<n;i++){
             if(colors[i])
                 continue;
-            if(bipartite(graph,i,colors))
+            if(bipartiteBFS(graph,i,colors))
                 return false;
         }
         return true;
