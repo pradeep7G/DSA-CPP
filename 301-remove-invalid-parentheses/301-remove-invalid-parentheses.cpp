5652945a67207e3173e3121c58d288128946078a
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-08-12 19:23:44 +0530

    Time: 155 ms (67.08%), Space: 32.2 MB (50.15%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -1,5 +1,57 @@
 class Solution {
 public:
+    void dfsPractice(int ind,int n,string &s,string validString,unordered_set<string> &res,int rmL,int rmR,int open){
+        if(open<0 || rmL<0 || rmR<0)
+             return ;
+        if(ind==n){
+            //reached end of the array
+            if(open==0 && rmL==0 && rmR==0){
+                res.insert(validString);
+            }
+            return ;
+        }
+        if(s[ind]=='('){
+            //not use
+            dfsPractice(ind+1,n,s,validString,res,rmL-1,rmR,open);
+            //use
+            dfsPractice(ind+1,n,s,validString+"(",res,rmL,rmR,open+1);
+        }
+        else if(s[ind]==')'){
+            //not use
+            dfsPractice(ind+1,n,s,validString,res,rmL,rmR-1,open);
+            // use
+            dfsPractice(ind+1,n,s,validString+")",res,rmL,rmR,open-1);
+        }
+        else{
+            dfsPractice(ind+1,n,s,validString+s[ind],res,rmL,rmR,open);
+        }
+    }
+    vector<string> bfsPractice(string s){
+        unordered_set<string> ht;
+        queue<string> q;
+        q.push(s);
+        vector<string> res;
+        while(q.size()){
+            string str=q.front();
+            q.pop();
+            if(ht.count(str))
+                continue;
+            ht.insert(str);
+            if(isValid(str)){
+                res.push_back(str);
+            }
+            else{
+                if(res.empty()){
+                    for(int i=0;i<str.length();i++){
+                        if(str[i]=='(' || str[i]==')'){
+                            q.push(str.substr(0,i)+str.substr(i+1));
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
     void solve(int ind,int n,string &s,string validString,unordered_set<string> &res,int rmL,int rmR,int open)
     {
         if(open<0 || rmL<0 || rmR<0)
@@ -43,7 +95,8 @@ public:
             }
         }
         unordered_set<string> res;
-        solve(0,n,s,"",res,rmL,rmR,0);
+        // solve(0,n,s,"",res,rmL,rmR,0);
+        dfsPractice(0,n,s,"",res,rmL,rmR,0);
         return vector<string>(res.begin(),res.end());//remove duplicates
     }
     bool isValid(string &s){
@@ -64,6 +117,8 @@ public:
         return count==0;
     }
     vector<string> removeInvalidParentheses(string s) {
+        //practice
+        return bfsPractice(s);
         //do bfs to generate valid paranthesis and once you find the solution then add them to the result
         //dfs ==> since unlike bfs we don't stop/know after removing parathesis which needs to be minimum so here first we found no.of excess brackets and then do bfs by removing/not removing and storing valid parenthesis;
         //finding excess left,right brackets
