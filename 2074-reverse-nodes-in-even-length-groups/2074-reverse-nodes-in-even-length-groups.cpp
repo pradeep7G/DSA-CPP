3e7b6dd8866a7d0bea7ecb078aaf40cd625aa322
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-09-12 14:46:49 +0530

    Time: 2340 ms (14.10%), Space: 329.8 MB (54.06%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
@@ -26,18 +26,9 @@ public:
                 }
                 return prev;
             }
-            else{
-                ListNode* pre=cur;
-                // while(len-- && cur){
-                //     cur=cur->next;
-                // }
-                return pre;
-            }
-            return NULL;
+            return cur;
         }
         if(id%2 == 1){
-            //continue;
-             cout<<cur->val<<"\n";
             int cnt=id;
             ListNode* prev=NULL;
             ListNode* pre=cur;
@@ -53,7 +44,6 @@ public:
         }
         else{
             //reverse in even length;
-            cout<<cur->val<<"\n";
             ListNode* prev=NULL;
             ListNode* pre=cur;
             int cnt=id;
@@ -70,6 +60,56 @@ public:
         }
         return head;
     }
+    
+    ListNode* practice(ListNode *head,int id,int len){
+        if(!head)
+            return NULL;
+        ListNode* cur=head;
+        if(len-id<0){
+            //reverse if even else return head;
+            if(len%2 == 0){
+                ListNode* prev=NULL;
+                while(len-- && cur){
+                    ListNode* nxt=cur->next;
+                    cur->next=prev;
+                    prev=cur;
+                    cur=nxt;
+                }
+                return prev;
+            }
+            return cur;
+        }
+        
+        //reverse based on group
+        if(id%2 == 0){
+            ListNode* prev=NULL;
+            ListNode* pre=cur;
+            int count=id;
+            while(count-- && cur){
+                ListNode* nxt=cur->next;
+                cur->next=prev;
+                prev=cur;
+                cur=nxt;
+            }
+            len-=id;
+            id++;
+            pre->next=practice(cur,id,len);
+            return prev;
+        }
+        else{
+            int count=id;
+            ListNode* pre=cur;
+            ListNode* prev=NULL;
+            while(cur && count--){
+                prev=cur;
+                cur=cur->next;
+            }
+            len-=id;
+            id++;
+            prev->next=practice(cur,id,len);
+            return pre;
+        }
+    }
     ListNode* reverseEvenLengthGroups(ListNode* head) {
         //iterative
         ListNode *cur=head;
@@ -78,79 +118,6 @@ public:
             len++;
             cur=cur->next;
         }
-        return solve(head,id,len);
-        // ListNode *prev=NULL,*next=NULL;
-        // cur=head;
-        // while(cur){
-        //    ListNode *odd=cur;
-        //    int skip=id,cnt=0;
-        //    //skip
-        //    int rem1=len-(skip*(skip+1))/2;
-        //    if(rem1>=0){
-        //         while(cnt<skip){
-        //            cnt++;
-        //            prev=odd;
-        //            odd=odd->next;
-        //        }
-        //        next=odd;
-        //    }
-        //    else{
-        //        if(((len-(skip*(skip-1))/2))%2==0){
-        //            //reverse
-        //            ListNode *lastprev=NULL;
-        //            while(odd){
-        //                ListNode* tmp=odd->next;
-        //                odd->next=lastprev;
-        //                lastprev=odd;
-        //                odd=tmp;
-        //             }
-        //            if(prev){
-        //                prev->next=lastprev;
-        //            }
-        //        }
-        //        return head;
-        //    }
-        //    if(next==NULL)
-        //         return head;
-        //    int reverse=id+1;cnt=0;
-        //    int rem=len-((reverse*(reverse+1))/2);
-        //    if(rem>=0){
-        //        //reverse
-        //        ListNode* pre=NULL;
-        //        ListNode* even=next;
-        //        int cnt=0;
-        //        while(cnt<reverse){
-        //            ListNode* tmp=even->next;
-        //            even->next=pre;
-        //            pre=even;
-        //            even=tmp;
-        //            cnt++;
-        //        }
-        //        if(prev)
-        //            prev->next=pre;
-        //        if(next)
-        //            next->next=even;
-        //        prev=;
-        //        cur=even;
-        //        id+=2;
-        //    }
-        //    else{
-        //        if(((len-((reverse*(reverse-1))/2))%2)==0){
-        //            //reverse
-        //            ListNode *lastprev=NULL;
-        //            while(next){
-        //                ListNode* tmp=next->next;
-        //                next->next=lastprev;
-        //                lastprev=next;
-        //                next=tmp;
-        //             }
-        //            if(prev){
-        //                prev->next=lastprev;
-        //            }
-        //        }
-        //        return head;
-        //    }
-        // }
-        // return head;
+        return practice(head,id,len);
     }
 };
