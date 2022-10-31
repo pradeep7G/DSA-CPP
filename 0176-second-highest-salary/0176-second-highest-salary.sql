89d9adede40657ff340ecdab9d7242a36f5b1edd
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-10-31 14:45:00 +0530

    Time: 553 ms (7.77%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,8 +2,14 @@
 # limit clause, nested queries, correlated-queries..
 
 #1
-# select s.salary as SecondHighestSalary from Employee s order by s.salary desc limit 1 offset 1;
+select (select distinct salary
+from Employee
+order by salary DESC
+limit 1 offset 1) as SecondHighestSalary;
 
+# NOTE: can't handle null values, might handle it with extra code like using if else etc.. but can try with nested queries with max aggrigate function.
 #2
 
-select max(s.salary) SecondHighestSalary from Employee s where s.salary <> (select max(t.salary) from Employee t);
+# select max(s.salary) SecondHighestSalary from Employee s where s.salary <> (select max(t.salary) from Employee t);
+
+# NOTE: max() returns null if no value exists
