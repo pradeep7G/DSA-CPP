acbe457acc35036f5d469cff76df6cb2178b3f6f
pradeep7G <56556561+pradeep7G@users.noreply.github.com>
2022-10-31 15:27:41 +0530

    Time: 2095 ms (7.96%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -4,6 +4,15 @@
 # Employee emp where emp.DepartmentId=dep.Id and 
 # (Select count(distinct Salary) From Employee where DepartmentId=dep.Id and Salary>emp.Salary)<3
 
-select dep.name as Department, emp.name as Employee, emp.salary from Department dep,Employee emp
-where emp.DepartmentId=dep.id and 
-(select count(distinct salary) from Employee where DepartmentId=dep.id and salary > emp.salary) < 3;
+# select dep.name as Department, emp.name as Employee, emp.salary from Department dep,Employee emp
+# where emp.DepartmentId=dep.id and 
+# (select count(distinct salary) from Employee where DepartmentId=dep.id and salary > emp.salary) < 3;
+
+#ranks
+
+select d.name Department, a.name Employee, a.salary from (select e.*, dense_rank() over (
+partition by DepartmentId order by salary desc
+) as DeptPayRank from Employee e) a
+join Department d
+on a.departmentId=d.id
+where DeptPayRank<=3;
